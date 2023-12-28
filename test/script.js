b81ded961334512dbc31d2ee44d0cfcc6c9ba938
4D-field <khk264@daum.net>
2023-12-28 21:24:21 +0900

    javascript update

---
+++ test/script.js
@@ -0,0 +1,38 @@
+
+          
+
+            document.addEventListener("DOMContentLoaded",
+                function(e){
+                    let keydown=document.querySelector("#keydown")
+                    keydown.addEventListener("keydown",
+                       function(e){
+                        console.log(e.target.value)
+                            } 
+                       )
+                
+            
+
+            let change=document.querySelector("#change")
+            change.addEventListener("change",
+                function(e) {
+                    console.log(e.target.value)
+                }
+            )
+
+            let input=document.querySelector("#input")
+            input.addEventListener("input",
+                function(e) {
+                    console.log(e.target.value)
+                       }
+                     )
+                }
+            )
+
+            function red(e) {
+                e.target.style.backgroundColor="red"
+            }
+
+            function blue(e) {
+                e.target.style.backgroundColor="blue"
+            }
+        
